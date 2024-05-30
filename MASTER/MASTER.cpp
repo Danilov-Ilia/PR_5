cc8e8267bba05070b36c458631eabb2f2576459d
Nick <polami3no@gmail.com>
2024-05-30 22:53:07 +0300

    first commit

---
+++ MASTER/MASTER.cpp
@@ -0,0 +1,27 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+int main()
+{
+    HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, TEXT("EventSync")); // создаем событие для синхронизации между процессами
+    if (hEvent == NULL) { // проверяем на ошибку открытия
+        printf("Error creating the event: %d\n", GetLastError());
+        return 1;
+    }
+
+    printf("MASTER: Started\n");
+
+    while (1) {  // бесконечный цикл для ожидания установки события и вывода "*"
+        WaitForSingleObject(hEvent, INFINITE); // ожидаем установку события
+
+        printf("*");
+
+        fflush(stdout); // принудительно очищаем буфер вывода
+
+        ResetEvent(hEvent); // сбрасываем событие после его использования
+    }
+
+    CloseHandle(hEvent); // закрываем дескриптор события
+
+    return 0;
+}
---
+++ SLAVE/SLAVE.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    HANDLE hEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EventSync")); // открываем событие для синхронизации между процессами
+    if (hEvent == NULL) { // проверяем на ошибку открытия
+        printf("Error opening the event: %d\n", GetLastError());
+        return 1;
+    }
+
+    printf("SLAVE: Started\n");
+
+    char c;
+    while (1) { // бесконечный цикл для ввода символов и установки события
+        c = _getch(); // считываем с клавиатуры символ без вывода на экран
+
+        printf("%c", c); // выводим на экран полученный с клавиатуры символ
+
+        SetEvent(hEvent); // устанавливаем событие
+
+        fflush(stdout); // принудительно очищаем буфер вывода
+
+    }
+
+    CloseHandle(hEvent); // закрываем дескриптор события
+
+    return 0;
+}
